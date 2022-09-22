80931435cc83ba868aaddf4fe5439ac01841cb81
Leandro Piovezan Dal Molin <leandro.swk@hotmail.com>
2022-09-23 00:45:00 +0100

    Add global styles and Header component

--- 01-react-fundamentals/src/App.jsx
+++ 01-react-fundamentals/src/App.jsx
@@ -1,7 +1,16 @@
-function App() {
+import { Header } from './components/Header';
+import { Post } from './Post';
+
+import './global.css';
+
+export function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <Header />
+      <Post
+        author="Leandro"
+        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut dolore ab iure necessitatibus, tempore nobis, exercitationem optio cumque magnam dolorem ipsum aspernatur quisquam omnis non sequi repellat eveniet molestias corrupti!"
+      />
+    </>
   )
 }
-
-export default App
