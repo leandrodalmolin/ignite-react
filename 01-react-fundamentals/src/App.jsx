735753e27ab962fbd9705b87c6e9ce2dcab24722
Leandro Piovezan Dal Molin <leandro.swk@hotmail.com>
2022-09-23 23:36:52 +0100

    Style sidebar

--- 01-react-fundamentals/src/App.jsx
+++ 01-react-fundamentals/src/App.jsx
@@ -1,16 +1,25 @@
 import { Header } from './components/Header';
 import { Post } from './Post';
 
+import styles from './App.module.css';
+
 import './global.css';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
     <>
       <Header />
-      <Post
-        author="Leandro"
-        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut dolore ab iure necessitatibus, tempore nobis, exercitationem optio cumque magnam dolorem ipsum aspernatur quisquam omnis non sequi repellat eveniet molestias corrupti!"
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Leandro"
+            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut dolore ab iure necessitatibus, tempore nobis, exercitationem optio cumque magnam dolorem ipsum aspernatur quisquam omnis non sequi repellat eveniet molestias corrupti!"
+          />
+        </main>
+      </div>
     </>
   )
 }
