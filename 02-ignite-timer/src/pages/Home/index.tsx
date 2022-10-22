482247df9086ec5170c9e85842a1a51a65cb358d
Leandro Piovezan Dal Molin <leandro.swk@hotmail.com>
2022-10-22 22:21:13 +0100

    Style home

---
+++ 02-ignite-timer/src/pages/Home/index.tsx
@@ -0,0 +1,59 @@
+import { Play } from 'phosphor-react'
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+} from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">I&apos;m gonna work on</label>
+          <TaskInput
+            type="text"
+            id="task"
+            placeholder="Give a title to your project"
+            list="task-suggestions"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Project 1" />
+            <option value="Project 2" />
+            <option value="Project 3" />
+          </datalist>
+
+          <label htmlFor="minutesAmount">for</label>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
+
+          <span>minutes.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton disabled type="submit">
+          <Play size={24} />
+          Start
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  )
+}
