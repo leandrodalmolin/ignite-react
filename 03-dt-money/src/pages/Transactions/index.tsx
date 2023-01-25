ef7d38877e86c3cb67644def64f79ef11ee97b42
Leandro Dal Molin <leandro.dalmolin@e3-342.local>
2023-01-25 23:26:12 +0000

    Style search form

--- 03-dt-money/src/pages/Transactions/index.tsx
+++ 03-dt-money/src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
@@ -9,27 +10,31 @@ export function Transactions() {
             <Summary />
 
             <TransactionsContainer>
+                <SearchForm />
+
                 <TransactionsTable>
-                    <tr>
-                        <td width="50%">Website development</td>
-                        <td>
-                            <PriceHighlight variant="income">
-                                £3,000.00
-                            </PriceHighlight>
-                        </td>
-                        <td>Sale</td>
-                        <td>25/01/2023</td>
-                    </tr>
-                    <tr>
-                        <td width="50%">Hamburger</td>
-                        <td>
-                            <PriceHighlight variant="outcome">
-                                - £10.00
-                            </PriceHighlight>
-                        </td>
-                        <td>Food</td>
-                        <td>20/01/2 023</td>
-                    </tr>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Website development</td>
+                            <td>
+                                <PriceHighlight variant="income">
+                                    £3,000.00
+                                </PriceHighlight>
+                            </td>
+                            <td>Sale</td>
+                            <td>25/01/2023</td>
+                        </tr>
+                        <tr>
+                            <td width="50%">Hamburger</td>
+                            <td>
+                                <PriceHighlight variant="outcome">
+                                    - £10.00
+                                </PriceHighlight>
+                            </td>
+                            <td>Food</td>
+                            <td>20/01/2 023</td>
+                        </tr>
+                    </tbody>
                 </TransactionsTable>
             </TransactionsContainer>
         </div>
