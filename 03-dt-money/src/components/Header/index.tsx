b9902b6405fdade24e253b4fa4162a0080be3ef5
Leandro Dal Molin <leandro.dalmolin@e3-342.e3media.co.uk>
2023-01-25 16:51:12 +0000

    Style header component

---
+++ 03-dt-money/src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+
+import logoSvg from '../../assets/logo.svg';
+
+export function Header() {
+    return (
+        <HeaderContainer>
+            <HeaderContent>
+                <img src={logoSvg} alt="" />
+                <NewTransactionButton>New transaction</NewTransactionButton>
+            </HeaderContent>
+        </HeaderContainer>
+    )
+}
