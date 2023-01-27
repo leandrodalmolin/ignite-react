235886df908af39ddd7bcad1e9bc0c97712d69da
Leandro Dal Molin <1050873+leandrodalmolin@users.noreply.github.com>
2023-01-27 17:19:00 +0000

    Install stitches and set it to load css when JS is disabled

--- 04-ignite-shop/src/pages/index.tsx
+++ 04-ignite-shop/src/pages/index.tsx
@@ -1,5 +1,11 @@
+import { styled } from "@/styles"
+
+const Button = styled('button', {
+  backgroundColor: '$rocketseat'
+})
+
 export default function Home() {
   return (
-    <h1>Ignite Shop</h1>
+    <Button>Send</Button>
   )
 }
