9161b0119ec3c57f966bedd6bd240b034470113b
Leandro Dal Molin <1050873+leandrodalmolin@users.noreply.github.com>
2023-01-30 14:03:35 +0000

    Create home page structure

--- 04-ignite-shop/src/pages/index.tsx
+++ 04-ignite-shop/src/pages/index.tsx
@@ -1,22 +1,30 @@
-import { styled } from "@/styles"
+import { HomeContainer, Product } from "@/styles/pages/home";
+import Image from "next/image";
 
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-
-  span: {
-    fontWeight: 'bold',
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.8)'
-  }
-})
+import tshirt1 from '@/assets/tshirts/1.png'
+import tshirt2 from '@/assets/tshirts/2.png'
+import tshirt3 from '@/assets/tshirts/3.png'
 
 export default function Home() {
   return (
-    <Button>Send</Button>
+    <HomeContainer>
+      <Product>
+        <Image src={tshirt1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>T-shirt 1</strong>
+          <span>£79.90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={tshirt2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>T-shirt 2</strong>
+          <span>£79.90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
