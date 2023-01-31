bc01df8912f31ba06ce7e567d226879152ad56cb
Leandro Dal Molin <1050873+leandrodalmolin@users.noreply.github.com>
2023-01-31 10:34:34 +0000

    Style single product page

--- 04-ignite-shop/src/pages/product/[id].tsx
+++ 04-ignite-shop/src/pages/product/[id].tsx
@@ -1,9 +1,23 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product"
 import { useRouter } from "next/router"
 
 export default function Product() {
     const { query } = useRouter()
 
     return (
-        <h1>Product: {JSON.stringify(query)}</h1>
+        <ProductContainer>
+            <ImageContainer>
+
+            </ImageContainer>
+
+            <ProductDetails>
+                <h1>T-shirt X</h1>
+                <span>£10.00</span>
+
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente corporis, vel sint est libero odio harum similique? Cupiditate, incidunt asperiores, commodi error, amet sapiente doloremque sit deleniti nesciunt omnis maiores?</p>
+
+                <button>Buy now</button>
+            </ProductDetails>
+        </ProductContainer>
     )
 }
