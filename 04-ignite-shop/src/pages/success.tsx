685cc3da12482e9ab185f8366f9aa3ff1bc60765
Leandro Dal Molin <1050873+leandrodalmolin@users.noreply.github.com>
2023-01-31 14:21:33 +0000

    Style success page

--- 04-ignite-shop/src/pages/success.tsx
+++ 04-ignite-shop/src/pages/success.tsx
@@ -1,5 +1,21 @@
+import { ImageContainer } from "@/styles/pages/success";
+import { SuccessContainer } from "@/styles/pages/success";
+import Link from "next/link";
+
 export default function Success() {
     return (
-        <h1>Success</h1>
+        <SuccessContainer>
+            <h1>Thank you for your order!</h1>
+
+            <ImageContainer>
+
+            </ImageContainer>
+
+            <p>
+                Woohoo <strong>Leandro</strong>, your <strong>t-shirt</strong> is already on its way.
+            </p>
+
+            <Link href="/">Back to catalog</Link>
+        </SuccessContainer>
     )
 }
