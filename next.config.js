d57b8a4a2d0b277dcfdfec4b20bdbac5472579e8
Leandro Dal Molin <1050873+leandrodalmolin@users.noreply.github.com>
2023-05-09 17:41:02 +0100

    Add "pageExtensions" to the Next.js config so we can have a style file without creating new routes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // allows us having a style file without creating a new route
+  pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
 }
 
 module.exports = nextConfig
