7c8b841bfcfd0209d973b69dfcf7a2f2c5a972b6
Leandro Dal Molin <1050873+leandrodalmolin@users.noreply.github.com>
2023-05-09 10:36:32 +0100

    Clean up - delete other projects

--- 02-ignite-timer/src/@types/styled.d.ts
+++
@@ -1,9 +0,0 @@
-import 'styled-components'
-import { defaultTheme } from '../styles/themes/default'
-
-type ThemeType = typeof defaultTheme
-
-// Suggests theme's colours on IDE's auto-completion
-declare module 'styled-components' {
-  export interface DefaultTheme extends ThemeType {}
-}
--- 03-dt-money/src/utils/formatter.ts
+++
@@ -1,6 +0,0 @@
-export const dateFormatter = new Intl.DateTimeFormat('en-GB')
-
-export const priceFormatter = new Intl.NumberFormat('en-BG', {
-  style: 'currency',
-  currency: 'GBP',
-})
