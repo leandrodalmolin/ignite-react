e83b071af5e80ae0e9f107f382c57659a0d7ff8f
Leandro Dal Molin <1050873+leandrodalmolin@users.noreply.github.com>
2023-05-10 15:25:31 +0100

    Add React Hook Form to ClaimUsernameForm

--- 06-ignite-call/src/pages/home/components/ClaimUsernameForm/index.tsx
+++ 06-ignite-call/src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,11 +1,30 @@
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
 import { Button, TextInput } from '@ldm-ignite-ui/react'
-import { Form } from './styles'
 import { ArrowRight } from 'phosphor-react'
+import { Form } from './styles'
+
+const claimUsernameFormSchema = z.object({
+  username: z.string(),
+})
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
 
 export function ClaimUsernameForm() {
+  const { register, handleSubmit } = useForm<ClaimUsernameFormData>()
+
+  async function handleUsernameClaim(data: ClaimUsernameFormData) {
+    console.log(data)
+  }
+
   return (
-    <Form as="form">
-      <TextInput size="sm" prefix="ignite.com/" placeholder="your-username" />
+    <Form as="form" onSubmit={handleSubmit(handleUsernameClaim)}>
+      <TextInput
+        size="sm"
+        prefix="ignite.com/"
+        placeholder="your-username"
+        {...register('username')}
+      />
       <Button size="sm" type="submit">
         Claim
         <ArrowRight />
