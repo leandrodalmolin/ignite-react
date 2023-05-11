1e6e6d7e30ad98c56d242c68b5ee15d35b861892
Leandro Dal Molin <1050873+leandrodalmolin@users.noreply.github.com>
2023-05-11 16:20:15 +0100

    Implement createUser method in the custom Prisma adapter

--- 06-ignite-call/README.md
+++ 06-ignite-call/README.md
@@ -7,12 +7,20 @@
 
 ### Custom adapter
 
-Please note that a custom Prisma adapter was created. Our flow is slightly different from the already existing prisma adapter.
+Please note that a custom Prisma adapter was created. Our flow is slightly different from the already existing Prisma adapter.
 
 In our case, we register our user in the `/register` route before the actual sign-in, where the user is registered in the original adapter.
 We also took the opportunity to normalise the fields' nomenclature.
 
 Due to these changes, we had to amend the next-auth types to accommodate the new Prisma structure (@types/next-auth.d.ts).
+
+### Auth flow
+
+- The user is added to the database during the registration
+- A cookie containing the user ID is set
+- On the Google Auth callback, the user is queried in the DB using the ID stored in the cookies
+- User data is updated with info coming from Google
+- Cookie is deleted
 
 ## Cookies
 
--- 06-ignite-call/src/pages/api/auth/[...nextauth].api.ts
+++ 06-ignite-call/src/pages/api/auth/[...nextauth].api.ts
@@ -1,33 +1,55 @@
+import { NextApiRequest, NextApiResponse } from 'next'
 import { PrismaAdapter } from '@/lib/auth/prisma-adapter'
 import NextAuth, { NextAuthOptions } from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
+import GoogleProvider, { GoogleProfile } from 'next-auth/providers/google'
 
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
-      authorization: {
-        params: {
-          scope:
-            'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar',
+export function buildNextAuthOptions(
+  req: NextApiRequest,
+  res: NextApiResponse,
+): NextAuthOptions {
+  return {
+    adapter: PrismaAdapter(req, res),
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+        authorization: {
+          params: {
+            scope:
+              'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar',
+          },
         },
-      },
-    }),
-  ],
-  callbacks: {
-    // check if permissions were given when user comes back from google
-    async signIn({ account }) {
-      if (
-        !account?.scope?.includes('https://www.googleapis.com/auth/calendar')
-      ) {
-        return '/register/connect-calendar/?error=permissions'
-      }
+        // Map new structure
+        profile: (profile: GoogleProfile) => {
+          return {
+            id: profile.sub,
+            name: profile.name,
+            username: '',
+            email: profile.email,
+            avatar_url: profile.picture,
+          }
+        },
+      }),
+    ],
+    callbacks: {
+      // check if permissions were given when user comes back from google
+      async signIn({ account }) {
+        if (
+          !account?.scope?.includes('https://www.googleapis.com/auth/calendar')
+        ) {
+          return '/register/connect-calendar/?error=permissions'
+        }
 
-      return true
+        return true
+      },
     },
-  },
+  }
 }
 
-export default NextAuth(authOptions)
+/**
+ * Pass response and request down to the auth options so we can have access to cookies
+ * Advanced initialization docs: https://authjs.dev/guides/basics/initialization#advanced-initialization
+ */
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  return await NextAuth(req, res, buildNextAuthOptions(req, res))
+}
