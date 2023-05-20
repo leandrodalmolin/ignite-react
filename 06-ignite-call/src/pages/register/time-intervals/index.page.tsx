2537c7690dd03ea1702a09a6c3bc2fa06821ad73
Leandro Dal Molin <1050873+leandrodalmolin@users.noreply.github.com>
2023-05-20 10:10:53 +0100

    Guarantee times have 1h distance on availability form (zod)

--- 06-ignite-call/src/pages/register/time-intervals/index.page.tsx
+++ 06-ignite-call/src/pages/register/time-intervals/index.page.tsx
@@ -3,6 +3,7 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { Controller, useFieldArray, useForm } from 'react-hook-form'
 import { ArrowRight } from 'phosphor-react'
 import { getWeekDays } from '@/utils/get-week-days'
+import { convertTimeStringToMinutes } from '@/utils/convert-time-string-to-minutes'
 import {
   Button,
   Checkbox,
@@ -33,14 +34,39 @@ const timeIntervalsFormSchema = z.object({
     )
     .length(7)
     // transform output to contain enabled days only
-    .transform((intervals) => intervals.filter((interval) => interval.enabled))
     // after data is transformed we need to use refine for validation
+    .transform((intervals) => intervals.filter((interval) => interval.enabled))
     .refine((intervals) => intervals.length > 0, {
-      message: 'You must select at least one day of the week',
-    }),
+      message: 'You must select at least one day of the week.',
+    })
+    // Guarantees times have 1h distance
+    .transform((intervals) => {
+      return intervals.map((interval) => {
+        return {
+          weekDay: interval.weekDay,
+          startTimeInMinutes: convertTimeStringToMinutes(interval.startTime),
+          endTimeInMinutes: convertTimeStringToMinutes(interval.endTime),
+        }
+      })
+    })
+    .refine(
+      (intervals) => {
+        return intervals.every((interval) => {
+          return interval.endTimeInMinutes - 60 >= interval.startTimeInMinutes
+        })
+      },
+      {
+        message: 'The end time must be at least 1h ahead of the start time.',
+      },
+    ),
 })
 
-type TimeIntervalsFormData = z.infer<typeof timeIntervalsFormSchema>
+// Due to the data transformation occurring in the schema, we must utilize
+// "z.input" and "z.output" instead of "z.infer" in order to obtain the types.
+// This is necessary to ensure that the "defaultValues" in the "useForm"
+// align with the schema before transformation.
+type TimeIntervalsFormInput = z.input<typeof timeIntervalsFormSchema>
+type TimeIntervalsFormOutput = z.output<typeof timeIntervalsFormSchema>
 
 export default function TimeIntervals() {
   const {
@@ -49,7 +75,7 @@ export default function TimeIntervals() {
     register,
     handleSubmit,
     formState: { isSubmitting, errors },
-  } = useForm({
+  } = useForm<TimeIntervalsFormInput>({
     resolver: zodResolver(timeIntervalsFormSchema),
     defaultValues: {
       intervals: [
@@ -73,8 +99,19 @@ export default function TimeIntervals() {
 
   const intervals = watch('intervals')
 
-  async function handleSetTimeIntervals(data: TimeIntervalsFormData) {
-    console.log(data)
+  // React Hook Form update has broken the output types, issue was raised here:
+  // - https://github.com/react-hook-form/react-hook-form/issues/9600
+  //
+  // For the version 7.44 (using 7.43.9 atm) they've implemented a new way of
+  // handling input and output types but it's not stable just yet.
+  // In the future we'll be able to use the below:
+  // useForm<TimeIntervalsFormInput, any, TimeIntervalsFormOutput>
+  //
+  // For now, as a workaround, we treat data as any and force the type
+  // inside the function preventing the type error on "handleSetTimeIntervals"
+  async function handleSetTimeIntervals(data: any) {
+    const formData = data as TimeIntervalsFormOutput
+    console.log(formData)
   }
 
   return (
