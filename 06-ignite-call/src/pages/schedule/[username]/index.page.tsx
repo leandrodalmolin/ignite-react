f320bed2ce6a9d25cc009de2b8bfed79e070fe13
Leandro Dal Molin <1050873+leandrodalmolin@users.noreply.github.com>
2023-05-21 09:54:35 +0100

    Start building booking page

---
+++ 06-ignite-call/src/pages/schedule/[username]/index.page.tsx
@@ -0,0 +1,58 @@
+import { Avatar, Heading, Text } from '@ldm-ignite-ui/react'
+import { Container, UserHeader } from './styles'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { prisma } from '@/lib/prisma'
+
+interface ScheduleProps {
+  user: {
+    name: string
+    bio: string
+    avatarUrl: string
+  }
+}
+
+export default function Schedule({ user }: ScheduleProps) {
+  return (
+    <Container>
+      <UserHeader>
+        <Avatar src={user.avatarUrl} />
+        <Heading>{user.name}</Heading>
+        <Text>{user.bio}</Text>
+      </UserHeader>
+    </Container>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [], // static pages will be generated only when user visits the page (not in the build)
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const username = String(params?.username)
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      user: {
+        name: user.name,
+        bio: user.bio,
+        avatarUrl: user.avatar_url,
+      },
+    },
+    revalidate: 60 * 60 * 24, // 1 day
+  }
+}
