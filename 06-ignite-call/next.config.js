2492df3daf791386b1c6195e6b28c0a39955069f
Leandro Dal Molin <1050873+leandrodalmolin@users.noreply.github.com>
2023-05-21 09:26:20 +0100

    Add google url to Next config to load profile pic (cors)

--- 06-ignite-call/next.config.js
+++ 06-ignite-call/next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   // allows us having a style file without creating a new route
   pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
+  // profile pic coming from Google
+  images: {
+    domains: ['https://lh3.googleusercontent.com/'],
+  },
 }
 
 module.exports = nextConfig
