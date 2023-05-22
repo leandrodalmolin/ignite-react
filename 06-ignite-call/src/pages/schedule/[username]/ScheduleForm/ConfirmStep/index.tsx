3edf343d15272d71f3c9a3848c3dbc0b2e6b0130
Leandro Dal Molin <1050873+leandrodalmolin@users.noreply.github.com>
2023-05-22 16:22:09 +0100

    Confirmation Step styles

--- 06-ignite-call/src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
+++ 06-ignite-call/src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -1,3 +1,44 @@
+import { Button, Text, TextArea, TextInput } from '@ldm-ignite-ui/react'
+import { CalendarBlank, Clock } from 'phosphor-react'
+import { ConfirmForm, FormActions, FormHeader } from './styles'
+
 export function ConfirmStep() {
-  return <div>Confirm Step</div>
+  function handleConfirmScheduling() {}
+
+  return (
+    <ConfirmForm as="form" onSubmit={handleConfirmScheduling}>
+      <FormHeader>
+        <Text>
+          <CalendarBlank />
+          22 September 2023
+        </Text>
+        <Text>
+          <Clock />
+          18:00h
+        </Text>
+      </FormHeader>
+
+      <label>
+        <Text size="sm">Full Name</Text>
+        <TextInput placeholder="Your name" />
+      </label>
+
+      <label>
+        <Text size="sm">Email Address</Text>
+        <TextInput type="email" placeholder="johndoe@example.com" />
+      </label>
+
+      <label>
+        <Text size="sm">Notes</Text>
+        <TextArea />
+      </label>
+
+      <FormActions>
+        <Button type="button" variant="tertiary">
+          Cancel
+        </Button>
+        <Button type="submit">Confirm</Button>
+      </FormActions>
+    </ConfirmForm>
+  )
 }
