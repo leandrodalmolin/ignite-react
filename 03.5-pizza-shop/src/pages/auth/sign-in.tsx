8f5700bf3e453d6c1d5027c077c16647260c4db5
Leandro Dal Molin <1050873+leandrodalmolin@users.noreply.github.com>
2024-03-19 16:18:03 +0000

    Setup sign in form with RHF and Zod

--- 03.5-pizza-shop/src/pages/auth/sign-in.tsx
+++ 03.5-pizza-shop/src/pages/auth/sign-in.tsx
@@ -1,8 +1,27 @@
 import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 
 import { Button, Input, Label } from '@/components'
 
+const signInForm = z.object({
+  email: z.string().email(),
+})
+
+type SignInForm = z.infer<typeof signInForm>
+
 export function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>()
+
+  async function handleSignIn(data: SignInForm) {
+    console.log(data)
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+  }
+
   return (
     <>
       <Helmet title="Login" />
@@ -18,13 +37,13 @@ export function SignIn() {
             </p>
           </div>
 
-          <form className="space-y-4">
+          <form className="space-y-4" onSubmit={handleSubmit(handleSignIn)}>
             <div className="space-y-2">
               <Label htmlFor="email">Your email</Label>
-              <Input id="email" type="email" />
+              <Input id="email" type="email" {...register('email')} />
             </div>
 
-            <Button className="w-full" type="submit">
+            <Button className="w-full" type="submit" disabled={isSubmitting}>
               Access dashboard
             </Button>
           </form>
