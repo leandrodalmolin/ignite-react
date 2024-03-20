6461d6e6a4e4363660f4fd44d308b5eba05b32fd
Leandro Dal Molin <1050873+leandrodalmolin@users.noreply.github.com>
2024-03-20 14:20:24 +0000

    Add loading state to account info requests

--- 03.5-pizza-shop/src/lib/axios.ts
+++ 03.5-pizza-shop/src/lib/axios.ts
@@ -6,3 +6,11 @@ export const api = axios.create({
   baseURL: env.VITE_API_URL,
   withCredentials: true, // send auth cookies in the request
 })
+
+// Debug only: add delay to requests to test loading states
+if (env.VITE_ENABLE_API_DELAY) {
+  api.interceptors.request.use(async (config) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+    return config
+  })
+}
