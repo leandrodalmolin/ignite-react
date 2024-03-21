9edf48cfbb32ee07de44d0a9d708d99e4f5fc238
Leandro Dal Molin <1050873+leandrodalmolin@users.noreply.github.com>
2024-03-21 12:52:35 +0000

    Add loading skeleton to the order details modal

--- 03.5-pizza-shop/src/pages/app/orders/order-details.tsx
+++ 03.5-pizza-shop/src/pages/app/orders/order-details.tsx
@@ -20,6 +20,8 @@ import {
   TableRow,
 } from '@/components/ui/table'
 
+import { OrderDetailsSkeleton } from './order-details-skeleton'
+
 export interface OrderDetailsProps {
   orderId: string
   open: boolean
@@ -41,7 +43,7 @@ export function OrderDetails({ orderId, open }: OrderDetailsProps) {
         <DialogDescription>Order details</DialogDescription>
       </DialogHeader>
 
-      {order && (
+      {order ? (
         <div className="space-y-6">
           <Table>
             <TableBody>
@@ -130,6 +132,8 @@ export function OrderDetails({ orderId, open }: OrderDetailsProps) {
             </TableFooter>
           </Table>
         </div>
+      ) : (
+        <OrderDetailsSkeleton />
       )}
     </DialogContent>
   )
