4e0a019319f51d14d86d63cdce1068378452f015
Leandro Dal Molin <1050873+leandrodalmolin@users.noreply.github.com>
2024-03-21 11:32:16 +0000

    Implement protected area access

--- 03.5-pizza-shop/src/pages/_layouts/app.tsx
+++ 03.5-pizza-shop/src/pages/_layouts/app.tsx
@@ -1,8 +1,38 @@
-import { Outlet } from 'react-router-dom'
+import { isAxiosError } from 'axios'
+import { useEffect } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
 
 import { Header } from '@/components'
+import { api } from '@/lib'
 
 export function AppLayout() {
+  const navigate = useNavigate()
+
+  // The backend authentication implementation employs HTTP-only cookies
+  // to safeguard private areas from potential manipulation or unauthorized access by extensions.
+  // Due to the unavailability of direct access to such cookies, interception of requests is necessary.
+  // If a user is unauthorized, they are redirected back to the login page.
+  useEffect(() => {
+    const interceptorId = api.interceptors.response.use(
+      (response) => response, // success
+      (error) => {
+        // isAxiosError give us access to the error types
+        if (isAxiosError(error)) {
+          const status = error.response?.status
+          const code = error.response?.data.code
+
+          if (status === 401 && code === 'UNAUTHORIZED') {
+            navigate('/sign-in', { replace: true })
+          }
+        }
+      },
+    )
+
+    return () => {
+      api.interceptors.response.eject(interceptorId)
+    }
+  }, [navigate])
+
   return (
     <div className="flex min-h-screen flex-col antialiased">
       <Header />
